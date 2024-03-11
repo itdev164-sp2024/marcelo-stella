2ee962b666e3f4844673230dcbcb4f662a865a0d
Marcelo Stella <Marcelo Stella>
2024-03-11 04:28:32 -0500

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -12,7 +12,7 @@ require('dotenv').config({
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `My New Real Blog!`,
     description: ``,
     author: `Marcelo Stella`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
